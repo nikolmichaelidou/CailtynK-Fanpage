aa48b0ef89f3d6b092032e33d252987654913243
nikolmichaelidou <nikolmichaelidou.nm@gmail.com>
2022-07-25 10:26:08 +0000

    Imported Flask and created index.html

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
-from flask import Flask 
+import os
+from flask import Flask, render_template
 
-app = flask (__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return
+    return render_template(index.html)
+
+    if __name__ == "__main__":
+        app.run(
+            host=os.environ.het("IP", "0.0.0.0"),
+            port=int(os.environ.get("PORT", "5000")),
+            debug=True)
