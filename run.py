1f42a6afd9a4b277868302a651bf56d60144bee8
nikolmichaelidou <nikolmichaelidou.nm@gmail.com>
2022-11-19 09:41:40 +0000

    added another HTML page

--- run.py
+++ run.py
@@ -31,6 +31,11 @@ def contact():
     return render_template("contact.html", page_title="Contact")
 
 
+@app.route("/missions")
+def missions():
+    render_template("missions.html", page_title="Her Story")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
